c7edbc4cc769eee5b1e17a6e09bdd734c1159fad
zlkca <likzhang@gmail.com>
2020-04-20 16:38:43 -0400

    refactory produsts

--- server/routers/product-route.ts
+++ server/routers/product-route.ts
@@ -1,20 +1,44 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import { DB } from "../db";
 import { Product } from "../models/product";
+import { Model } from "../models/model";
 
 export function ProductRouter(db: DB){
   const router = express.Router();
-  const controller = new Product(db);
-
-  router.get('/qFind', (req, res) => { controller.quickFind(req, res); });
-  router.get('/clearImage', (req, res) => { controller.clearImage(req, res); });
-  router.get('/categorize', (req, res) => { controller.categorize(req, res); });
+  const model = new Product(db);
+  const controller = new ProductController(db);
+  
   router.get('/', (req, res) => { controller.list(req, res); });
-  router.get('/:id', (req, res) => { controller.get(req, res); });
-  router.post('/', (req, res) => { controller.create(req, res); });
-  router.put('/', (req, res) => { controller.replace(req, res); });
-  router.patch('/', (req, res) => { controller.update(req, res); });
-  router.delete('/', (req, res) => { controller.remove(req, res); });
+
+  router.get('/qFind', (req, res) => { model.quickFind(req, res); });
+  router.get('/clearImage', (req, res) => { model.clearImage(req, res); });
+  router.get('/categorize', (req, res) => { model.categorize(req, res); });
+  router.get('/:id', (req, res) => { model.get(req, res); });
+  router.post('/', (req, res) => { model.create(req, res); });
+  router.put('/', (req, res) => { model.replace(req, res); });
+  router.patch('/', (req, res) => { model.update(req, res); });
+  router.delete('/', (req, res) => { model.remove(req, res); });
 
   return router;
 };
+
+class ProductController extends Model{
+  model: Product;
+
+  constructor(db: DB) {
+    super(db, 'products');
+    this.model = new Product(db);
+  }
+
+  list(req: Request, res: Response) {
+    let query = {};
+    if (req.headers && req.headers.filter && typeof req.headers.filter === 'string') {
+      query = (req.headers && req.headers.filter) ? JSON.parse(req.headers.filter) : null;
+    }
+
+    this.model.list(query).then((ps: any[]) => {
+      res.setHeader('Content-Type', 'application/json');
+      res.send(JSON.stringify(ps, null, 3));
+    });
+  }
+}
