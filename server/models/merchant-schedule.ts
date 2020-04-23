b5fe1454af088ad6c2c5dcb1c186c7a13a33e05a
zlkca <likzhang@gmail.com>
2020-04-23 11:42:26 -0400

    update merchant-schedule api

--- server/models/merchant-schedule.ts
+++ server/models/merchant-schedule.ts
@@ -11,6 +11,51 @@ export class MerchantSchedule extends Model{
     this.areaModel = new Area(dbo);
   }
 
+  createOrUpdate(data: any){
+    const areaId = data.areaId;
+    const areaCode = data.areaCode;
+    const merchantIds = data.merchantIds; // all merchants
+    const weeks = data.weeks.split(',');
+    const rules = weeks.map((week: string) => ({
+        pickup: {dow: week, time: '10:00'},
+        deliver: {dow: week, time: '10:00'},
+      }));
+    const existMerchantIds: string[] = [];
+    const datas: any[] = [];
+
+    return new Promise(resolve => {
+      this.find({areaId}).then(mss => {
+        mss.forEach(ms => {
+          existMerchantIds.push(ms.merchantId.toString());
+          datas.push({
+            query: { _id: ms._id },
+            data: { areaId, areaCode, rules }
+          });
+        });
+        this.bulkUpdate(datas).then(() => {
+          const remainIds = merchantIds.filter((mId: string) => existMerchantIds.indexOf(mId) === -1);
+          this.createMany(areaId, areaCode, rules, remainIds);
+          resolve();
+        });
+      });
+    });
+  }
+
+  createMany(areaId: string, areaCode: string, rules: any[], ids: string[]){
+    for(let i=0; i<ids.length; i++){
+      const merchantId = ids[i];
+      const data = {
+        areaId,
+        areaCode,
+        merchantId,
+        rules
+      }
+      this.insertOne(data).then(() => {
+
+      });
+    }
+  }
+
   // v1
   getAvailableSchedules(req: Request, res: Response) {
     let fields: any;
